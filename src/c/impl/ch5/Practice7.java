29ee8f5c59c8fe6ab8b57cc2a60b079507d6bdf1
Gou <gouyikui@internal.baidu.com>
2016-10-11 15:34:11 +0800

    ch5.7

--- src/c/impl/ch5/Practice7.java
+++ src/c/impl/ch5/Practice7.java
@@ -2,14 +2,28 @@ package c.impl.ch5;
 
 /*
  * practice 题目：
-
- */
+求爱因斯坦数学题。有一条长阶，
+若每步跨2阶，则最后剩1阶，
+若每步跨3阶，则最后剩2阶，
+若每步跨5阶，则最后剩4阶，
+若每步跨6阶，则最后剩5阶，
+若每步跨7阶，最后正好1阶。
+*/
 public class Practice7 {
 
-	
+	public static void compute(){
+		int x = 0;
+		while(true){
+			if(x%2==1 && x%3==2 && x%5==4 && x%6==5 && x%7==1){
+				System.out.println(x);
+				break;
+			}
+			x++;
+		}
+	}
 	
 	public static void main(String[] args) {
-
+		compute();
 	}
 
 }
